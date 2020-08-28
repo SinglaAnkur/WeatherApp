ae108a08cb0cda78cf6834d06d4cbd2ec5dfdaa7
unknown <singla.ankur147@gmail.com>
2020-08-28 15:38:31 -0400

    Displaying Temperature of Montreal

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const url = 'http://api.weatherstack.com/current?access_key=b45b89c9d9827638b34862bde443e05d&query=45.508888,-73.561668'
 
-request({url : url}, (error,response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url : url, json:true}, (error,response) => {
+    console.log('It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' +
+    response.body.current.feelslike + ' degrees out.')
 })
