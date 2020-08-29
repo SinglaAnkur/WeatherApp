f858f7296f5362e8ab3cf14b3c93b080ffed110d
unknown <singla.ankur147@gmail.com>
2020-08-29 19:59:18 -0400

    Displaying the temperature in Fahrenheit and weather desription

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const request = require('request')
-const url = 'http://api.weatherstack.com/current?access_key=b45b89c9d9827638b34862bde443e05d&query=45.508888,-73.561668'
+const url = 'http://api.weatherstack.com/current?access_key=b45b89c9d9827638b34862bde443e05d&query=45.508888,-73.561668&units=f'
 
 request({url : url, json:true}, (error,response) => {
-    console.log('It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' +
+    console.log(response.body.current.weather_descriptions + '. It is currently ' + 
+    response.body.current.temperature + ' degrees out. It feels like ' + 
     response.body.current.feelslike + ' degrees out.')
 })
