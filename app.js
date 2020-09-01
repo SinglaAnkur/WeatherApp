750d1c45e8773b0fa4ecccd902402256ad0e4102
unknown <singla.ankur147@gmail.com>
2020-09-01 11:51:30 -0400

    Moved Geocode to separate file in utils folder

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 const url = 'http://api.weatherstack.com/current?access_key=b45b89c9d9827638b34862bde443e05d&query=45.508888,-73.561668&units=f'
 
 request({url : url, json:true}, (error,response) => {
@@ -15,25 +16,7 @@ request({url : url, json:true}, (error,response) => {
     }
 })
 
-const geocodeURL = (address,callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5rdXJzaW5nbGEiLCJhIjoiY2tlbHI2bWxwMDJ5eDJxcGNzMTNxeDQxeCJ9.yCeO0x1iWYB5jJoZhtZTzg&limit=1'
-    request({url:url, json:true}, (error,response) => {
-        if(error) {
-            callback('Unable to connect to location services', undefined)
-        } else if(response.body.features.length === 0) {
-            callback('Unable to find the location! Try again', undefined)
-        } else {
-            const longitude = response.body.features[0].center[0]
-            const latitude = response.body.features[0].center[1]
-            callback(undefined, {
-                longitude: longitude,
-                latitude: latitude
-            })
-        }     
-    })
-}
-
-geocodeURL('Montreal', (error, data) => {
+geocode('Montreal', (error, data) => {
     console.log('Error: ' + error)
     console.log('Data:', data)
 })
