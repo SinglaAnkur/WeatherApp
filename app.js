0b0080daef2e0d4b5581cdadad353772066d1569
unknown <singla.ankur147@gmail.com>
2020-09-01 11:43:07 -0400

    Geolocation Callback Abstraction

--- app.js
+++ app.js
@@ -15,17 +15,25 @@ request({url : url, json:true}, (error,response) => {
     }
 })
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Montreal.json?access_token=pk.eyJ1IjoiYW5rdXJzaW5nbGEiLCJhIjoiY2tlbHI2bWxwMDJ5eDJxcGNzMTNxeDQxeCJ9.yCeO0x1iWYB5jJoZhtZTzg&limit=1'
+const geocodeURL = (address,callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5rdXJzaW5nbGEiLCJhIjoiY2tlbHI2bWxwMDJ5eDJxcGNzMTNxeDQxeCJ9.yCeO0x1iWYB5jJoZhtZTzg&limit=1'
+    request({url:url, json:true}, (error,response) => {
+        if(error) {
+            callback('Unable to connect to location services', undefined)
+        } else if(response.body.features.length === 0) {
+            callback('Unable to find the location! Try again', undefined)
+        } else {
+            const longitude = response.body.features[0].center[0]
+            const latitude = response.body.features[0].center[1]
+            callback(undefined, {
+                longitude: longitude,
+                latitude: latitude
+            })
+        }     
+    })
+}
 
-request({url:geocodeURL, json:true}, (error,response) => {
-    if(error) {
-        console.log('Unable to connect to location services')
-    } else if(response.body.features.length === 0) {
-        console.log('Unable to find the location! Try again')
-    } else {
-        const longitude = response.body.features[0].center[0]
-        const latitude = response.body.features[0].center[1]
-        console.log(latitude, longitude)
-    }
-    
+geocodeURL('Montreal', (error, data) => {
+    console.log('Error: ' + error)
+    console.log('Data:', data)
 })
