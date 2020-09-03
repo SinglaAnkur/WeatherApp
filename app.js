abbadc6ed09d9f2700d54c77a009f1586f42573b
unknown <singla.ankur147@gmail.com>
2020-09-03 15:38:25 -0400

    Provide weather forecast for any given user input

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Montreal', (error, data) => {
-    if(error) {
-        return console.log(error)
-    }
+const address = process.argv[2]
 
-    forecast(45.5088, -73.554, (error, forecastData) => {
+if(!address) {
+    console.log("Please provide an address")
+} else {
+    geocode(address, (error, data) => {
         if(error) {
             return console.log(error)
         }
-        console.log(data.location)
-        console.log(forecastData)
-      })
-})
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error) {
+                return console.log(error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+          })
+    })
+}
