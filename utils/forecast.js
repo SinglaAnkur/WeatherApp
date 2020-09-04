8f0b5600dcef04232fdd00d034336c82023b9afa
unknown <singla.ankur147@gmail.com>
2020-09-04 16:04:38 -0400

    Changes for property shorthand and object destructuring in forecast.js

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,15 +2,15 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=b45b89c9d9827638b34862bde443e05d&query=' + latitude + ',' + longitude
    
-    request({url:url, json:true}, (error, response) => {
+    request({url, json:true}, (error, {body}) => {
         if(error) {
             callback('Unable to connect to weather application', undefined)
-        } else if(response.body.error) {
+        } else if(body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions + 
-                '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' 
-                + response.body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions + 
+                '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' 
+                + body.current.feelslike + ' degrees out.')
         }
     })
 
